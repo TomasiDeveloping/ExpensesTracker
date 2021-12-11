943010c1f0dd14d12ba54cbf82f3b9f2c8777f79
Patrick Tomasi <info@tomasi-developing.ch>
2021-12-11 16:51:07 +0100

    Basic structure

--- ExpensesTracker/ClientApp/src/app/app.module.ts
+++ ExpensesTracker/ClientApp/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import {AppRoutingModule} from "./app-routing.module";
+import { CategoriesComponent } from './categories/categories.component';
+import { ExpendituresComponent } from './expenditures/expenditures.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    CategoriesComponent,
+    ExpendituresComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
