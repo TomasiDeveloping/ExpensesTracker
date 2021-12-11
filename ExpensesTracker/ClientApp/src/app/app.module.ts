11f4b619f68f8e4246fd7cb0f7963ca3d3c8b249
Patrick Tomasi <info@tomasi-developing.ch>
2021-12-11 15:34:24 +0100

    @types/jasmine

--- ExpensesTracker/ClientApp/src/app/app.module.ts
+++ ExpensesTracker/ClientApp/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserModule,
     HttpClientModule,
     FormsModule
   ],
