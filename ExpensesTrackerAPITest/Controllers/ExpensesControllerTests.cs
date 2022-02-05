f10b81afe9d451d2da86ae4139f30dbb2eb5ddc6
Tomasi - Developing <info@tomasi-developing.ch>
2022-02-05 12:17:21 +0100

    Add Tests

---
+++ ExpensesTrackerAPITest/Controllers/ExpensesControllerTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.DTOs;
+using Core.Interfaces;
+using ExpensesTracker.Controllers.v1;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace ExpensesTrackerAPITest.Controllers
+{
+    public class ExpensesControllerTests
+    {
+        private readonly Mock<IExpenseService> _expenseServiceMock;
+        private readonly ExpensesController _expensesController;
+        public ExpensesControllerTests()
+        {
+            _expenseServiceMock = new Mock<IExpenseService>();
+            _expensesController = new ExpensesController(_expenseServiceMock.Object);
+        }
+
+        [Fact]
+        public async void ExpensesController_Get()
+        {
+            var testExpenses = GetExpenses();
+            _expenseServiceMock.Setup(x => x.GetExpensesAsync())
+                .ReturnsAsync(testExpenses);
+
+            var response = await _expensesController.Get() as ObjectResult;
+            var expenses = response?.Value as List<ExpenseDto>;
+
+            Assert.IsType<List<ExpenseDto>>(expenses);
+            Assert.Equal(2, expenses?.Count);
+            Assert.Equal("Test 2", expenses?[1].Description);
+        }
+
+        [Theory]
+        [InlineData(1, 200)]
+        [InlineData(2, 400)]
+        public async void ExpensesController_GetExpensesByIdAsync(int expenseId, int statusCode)
+        {
+            var testExpense = GetExpenses().First(x => x.Id == 1);
+            _expenseServiceMock.Setup(x => x.GetExpenseByIdAsync(1))
+                .ReturnsAsync(testExpense);
+
+            var response = await _expensesController.Get(expenseId) as ObjectResult;
+
+            Assert.Equal(statusCode, response?.StatusCode);
+        }
+
+        [Fact]
+        public async void ExpensesController_GetUsersExpensesByParamsAsync()
+        {
+            var testExpenses = GetExpenses();
+            _expenseServiceMock.Setup(x => x.GetUserExpensesByParamsAsync(1, 2022, 1))
+                .ReturnsAsync(testExpenses);
+
+            var response = await _expensesController.GetExpensesByUserId(1, 2022, 1) as ObjectResult;
+            var userExpenses = response?.Value as List<ExpenseDto>;
+
+            Assert.Equal(200, response?.StatusCode);
+            Assert.Equal(2, userExpenses?.Count);
+        }
+
+        private static List<ExpenseDto> GetExpenses()
+        {
+            return new List<ExpenseDto>
+            {
+                new()
+                {
+                    Amount = 20,
+                    CategoryId = 1,
+                    CategoryName = "Test",
+                    Description = "Test 1",
+                    CreateDate = new DateTime(2022, 1, 1),
+                    Id = 1,
+                    UserId = 1
+                },
+                new()
+                {
+                    Amount = 10,
+                    CategoryId = 1,
+                    CategoryName = "Test",
+                    Description = "Test 2",
+                    CreateDate = new DateTime(2022, 1, 2),
+                    Id = 2,
+                    UserId = 1
+                }
+            };
+        }
+    }
+}
---
+++ ExpensesTrackerAPITest/Controllers/UsersControllerTest.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Core.DTOs;
+using Core.Helper.Classes;
+using Core.Interfaces;
+using ExpensesTracker.Controllers.v1;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Moq;
+using Xunit;
+
+namespace ExpensesTrackerAPITest.Controllers
+{
+    public class UsersControllerTest
+    {
+        private readonly Mock<IUserService> _mockRepo;
+        private readonly UsersController _usersController;
+
+        public UsersControllerTest()
+        {
+            var settings = Options.Create(new EmailSettings());
+            _mockRepo = new Mock<IUserService>();
+            _usersController = new UsersController(_mockRepo.Object, settings);
+        }
+
+        [Fact]
+        public async void UserController_GetUserById_Whit_ValidUser()
+        {
+            const int userId = 1;
+            var user = GetTestUser();
+            _mockRepo.Setup(x => x.GetUserByIdAsync(userId))
+                .ReturnsAsync(user);
+
+            var result = await _usersController.Get(user.Id) as ObjectResult;
+
+            var testUser = Assert.IsType<UserDto>(result?.Value);
+            Assert.NotNull(testUser);
+            Assert.Equal(user.FirstName, testUser.FirstName);
+            Assert.Equal(user.LastName, testUser.LastName);
+            Assert.Equal(user.IsActive, testUser.IsActive);
+        }
+
+        [Fact]
+        public async void UserController_PostAsync_InsertUser()
+        {
+            var user = GetTestUser();
+            UserDto? testUser = null;
+            _mockRepo.Setup(x => x.InsertUserAsync(It.IsAny<UserDto>()))
+                .Callback<UserDto>(x => testUser = x );
+
+            await _usersController.Post(user);
+            _mockRepo.Verify(x => x.InsertUserAsync(It.IsAny<UserDto>()), Times.Once);
+
+            Assert.NotNull(testUser);
+            Assert.Equal(user.FirstName, testUser?.FirstName);
+            Assert.Equal(user.LastName, testUser?.LastName);
+            _mockRepo.Verify();
+
+        }
+
+        [Fact]
+        public async void UserController_PutAsync_UpdateUser()
+        {
+            var testUser = GetTestUser();
+            _mockRepo.Setup(x => x.UpdateUserAsync(1, It.IsAny<UserDto>()))
+                .ReturnsAsync(testUser);
+            testUser.LastName = "Update";
+
+            var result = await _usersController.Put(testUser.Id, testUser) as ObjectResult;
+
+            var updatedUser = result?.Value as UserDto;
+            Assert.Equal(200, result?.StatusCode);
+            Assert.IsType<UserDto>(updatedUser);
+            Assert.Equal(testUser.LastName, updatedUser?.LastName);
+
+        }
+
+        [Fact]
+        public async void UserController_DeleteAsync_DeleteUserSuccessfully()
+        {
+            var testUser = GetTestUser();
+            _mockRepo.Setup(x => x.DeleteUserAsync(1))
+                .ReturnsAsync(true);
+
+            var result = await _usersController.Delete(testUser.Id) as ObjectResult;
+            Assert.NotNull(result);
+            Assert.Equal(200, result?.StatusCode);
+            Assert.True((bool)(result?.Value ?? false));
+        }
+
+        private static UserDto GetTestUser()
+        {
+            return new UserDto()
+            {
+                CreatedAt = DateTime.Now,
+                Email = "test@test.com",
+                FirstName = "John",
+                LastName = "Doe",
+                IsActive = true,
+                MonthlyBudget = 1000,
+                Password = "Test",
+                WithRevenue = true,
+                Id = 1
+            };
+        }
+    }
+}
