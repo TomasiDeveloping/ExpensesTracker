6753bfef76ebb7239b225f6c048e9e8974955fb2
Tomasi - Developing <info@tomasi-developing.ch>
2023-02-18 20:10:48 +0100

    Refactoring Program.cs

---
+++ ExpensesTracker/Configurations/ServiceExtensions.cs
@@ -0,0 +1,106 @@
+﻿using System.Text;
+using DataBase;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+namespace ExpensesTracker.Configurations;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Expense Tracker API",
+                Version = "v1",
+                Description = "API for Expense Tracker",
+                Contact = new OpenApiContact
+                {
+                    Email = "info@tomasi-developing.ch",
+                    Name = "Tomasi-Developing",
+                    Url = new Uri("https://tomasi-developing.ch")
+                }
+            });
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = @"JWT Authorization header using the Bearer scheme.
+                        Enter 'Bearer' [space] and then your token in the text input below.
+                        Example: 'Bearer 12345abcdef'",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = JwtBearerDefaults.AuthenticationScheme
+            });
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = JwtBearerDefaults.AuthenticationScheme
+                        },
+                        Scheme = "Oauth2",
+                        Name = JwtBearerDefaults.AuthenticationScheme,
+                        In = ParameterLocation.Header
+                    },
+                    new List<string>()
+                }
+            });
+        });
+    }
+
+    public static void ConfigureApiVersioning(this IServiceCollection services)
+    {
+        services.AddApiVersioning(option =>
+        {
+            option.DefaultApiVersion = new ApiVersion(1, 0);
+            option.AssumeDefaultVersionWhenUnspecified = true;
+            option.ReportApiVersions = true;
+        });
+    }
+
+    public static void ConfigureAuthentication(this IServiceCollection services, IConfigurationSection jwtSettings)
+    {
+        services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]!)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+    }
+
+    public static void ConfigureCors(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAll", policyBuilder =>
+                policyBuilder.AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowAnyOrigin());
+        });
+    }
+
+    public static void ConfigureDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ExpensesTrackerContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        });
+    }
+}
--- ExpensesTracker/Program.cs
+++ ExpensesTracker/Program.cs
@@ -1,15 +1,9 @@
-using System.Text;
 using Core.Helper.Classes;
 using Core.Helper.Services;
 using Core.Interfaces;
-using DataBase;
 using DataBase.Profiles;
 using DataBase.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
+using ExpensesTracker.Configurations;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,51 +14,14 @@ builder.Host.UseSerilog((context, configuration) =>
         .ReadFrom.Configuration(context.Configuration);
 });
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo {Title = "Expense Tracker", Version = "v1"});
-
-    var securitySchema = new OpenApiSecurityScheme
-    {
-        Description = "JWT Auth Bearer Scheme",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-        Scheme = "bearer",
-        Reference = new OpenApiReference
-        {
-            Type = ReferenceType.SecurityScheme,
-            Id = "Bearer"
-        }
-    };
-
-    c.AddSecurityDefinition("Bearer", securitySchema);
-    var securityRequirement = new OpenApiSecurityRequirement
-    {
-        {
-            securitySchema, new[]
-                {"Bearer"}
-        }
-    };
-    c.AddSecurityRequirement(securityRequirement);
-});
-
-builder.Services.AddApiVersioning(option =>
-{
-    option.DefaultApiVersion = new ApiVersion(1, 0);
-    option.AssumeDefaultVersionWhenUnspecified = true;
-    option.ReportApiVersions = true;
-});
-
+builder.Services.ConfigureCors();
+var jwtSettings = builder.Configuration.GetSection("Token");
+builder.Services.ConfigureAuthentication(jwtSettings);
+builder.Services.ConfigureSwagger();
+builder.Services.ConfigureApiVersioning();
 builder.Services.AddAutoMapper(typeof(AutoMapperConfig));
-
-builder.Services.AddDbContext<ExpensesTrackerContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+builder.Services.ConfigureDbContext(builder.Configuration);
 
 builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddScoped<IUserService, UserService>();
@@ -77,27 +34,10 @@ builder.Services.AddScoped<IRecurringTaskService, RecurringTaskService>();
 builder.Services.AddScoped<ICronService, CronService>();
 builder.Services.AddScoped<IApplicationVersionConfirmationService, ApplicationVersionConfirmationService>();
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Token:Key"]!)),
-            ValidateIssuer = false,
-            ValidateAudience = false
-        };
-    });
-
 builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", policyBuilder =>
-        policyBuilder.AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowAnyOrigin());
-});
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
