b067885e1ef8142df0b6051fe175192d3ad1631f
Tomasi - Developing <info@tomasi-developing.ch>
2023-04-23 18:51:43 +0200

    CleanUp and Refactoring recurringTasks

--- ExpensesTracker/ClientApp/src/app/recurring-task/edit-recurring-task/edit-recurring-task.component.ts
+++ ExpensesTracker/ClientApp/src/app/recurring-task/edit-recurring-task/edit-recurring-task.component.ts
@@ -1,6 +1,6 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component, inject, Inject, OnInit} from '@angular/core';
 import {RecurringTask} from "../../models/recurringTask.model";
-import {FormControl, FormGroup, UntypedFormControl, Validators} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {RevenueCategoryService} from "../../services/revenue-category.service";
 import {CategoriesService} from "../../services/categories.service";
 import {RecurringTaskService} from "../../services/recurring-task.service";
@@ -14,23 +14,48 @@ import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 })
 export class EditRecurringTaskComponent implements OnInit {
 
-  currentRecurringTask: RecurringTask;
-  recurringTaskForm!: FormGroup;
-  categoryGroups: { id: number, name: string }[] = [];
-  executeMonths: { value: number, description: string }[] = [
+  public currentRecurringTask: RecurringTask;
+  public recurringTaskForm!: FormGroup;
+  public categoryGroups: { id: number, name: string }[] = [];
+  public executeMonths: { value: number, description: string }[] = [
     {value: 1, description: 'Monatlich'},
     {value: 3, description: 'Vierteljährlich'},
     {value: 6, description: 'Halbjährlich'},
     {value: 12, description: 'Jährlich'}
   ];
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any,
-              private dialogRef: MatDialogRef<EditRecurringTaskComponent>,
-              private recurringTaskService: RecurringTaskService,
-              private revenueCategoryService: RevenueCategoryService,
-              private toastr: ToastrService,
-              private expenseCategoryService: CategoriesService) {
+  private readonly _dialogRef = inject(MatDialogRef<EditRecurringTaskComponent>);
+  private readonly _recurringTaskService = inject(RecurringTaskService);
+  private readonly _revenueCategoryService = inject(RevenueCategoryService);
+  private readonly _toastr = inject(ToastrService);
+  private readonly _expenseCategoryService = inject(CategoriesService);
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
     this.currentRecurringTask = data.recurringTask;
+  }
+
+  get amount() {
+    return this.recurringTaskForm.get('amount')!;
+  }
+
+  get categoryId() {
+    return this.recurringTaskForm.get('categoryId')!;
+  }
+
+  get revenueCategoryId() {
+    return this.recurringTaskForm.get('revenueCategoryId')!;
+  }
+
+  get description() {
+    return this.recurringTaskForm.get('description')!;
+  }
+
+  get executeInMonths() {
+    return this.recurringTaskForm.get('executeInMonths')!;
+  }
+
+  get nextExecution() {
+    return this.recurringTaskForm.get('nextExecution')!;
   }
 
   ngOnInit(): void {
@@ -46,31 +71,31 @@ export class EditRecurringTaskComponent implements OnInit {
   createForm() {
     const date = new Date(this.currentRecurringTask.nextExecution);
     this.recurringTaskForm = new FormGroup<any>({
-      id: new FormControl(this.currentRecurringTask.id, [Validators.required]),
-      userId: new FormControl(this.currentRecurringTask.userId, [Validators.required]),
-      isActive: new FormControl(this.currentRecurringTask.isActive, [Validators.required]),
-      isRevenue: new FormControl(this.currentRecurringTask.isRevenue, [Validators.required]),
-      isExpense: new FormControl(this.currentRecurringTask.isExpense, [Validators.required]),
-      categoryId: new FormControl(this.currentRecurringTask.categoryId),
-      expenseCategoryName: new FormControl(this.currentRecurringTask.expenseCategoryName),
-      revenueCategoryId: new FormControl(this.currentRecurringTask.revenueCategoryId),
-      revenueCategoryName: new FormControl(this.currentRecurringTask.revenueCategoryName),
-      lastExecution: new UntypedFormControl(this.currentRecurringTask.lastExecution),
-      nextExecution: new UntypedFormControl(new Date(
+      id: new FormControl<number>(this.currentRecurringTask.id, [Validators.required]),
+      userId: new FormControl<number>(this.currentRecurringTask.userId, [Validators.required]),
+      isActive: new FormControl<boolean>(this.currentRecurringTask.isActive, [Validators.required]),
+      isRevenue: new FormControl<boolean>(this.currentRecurringTask.isRevenue, [Validators.required]),
+      isExpense: new FormControl<boolean>(this.currentRecurringTask.isExpense, [Validators.required]),
+      categoryId: new FormControl<number | undefined>(this.currentRecurringTask.categoryId),
+      expenseCategoryName: new FormControl<string | undefined>(this.currentRecurringTask.expenseCategoryName),
+      revenueCategoryId: new FormControl<number | undefined>(this.currentRecurringTask.revenueCategoryId),
+      revenueCategoryName: new FormControl<string>(this.currentRecurringTask.revenueCategoryName),
+      lastExecution: new FormControl<Date>(this.currentRecurringTask.lastExecution),
+      nextExecution: new FormControl<string>(new Date(
         Date.UTC(date.getFullYear(),
           date.getMonth(),
           date.getDate()))
         .toISOString().substring(0, 10)
       ),
-      executeInMonths: new FormControl(this.currentRecurringTask.executeInMonths, [Validators.required]),
-      amount: new FormControl(this.currentRecurringTask.amount, [Validators.required]),
-      description: new FormControl(this.currentRecurringTask.description)
+      executeInMonths: new FormControl<number>(this.currentRecurringTask.executeInMonths, [Validators.required]),
+      amount: new FormControl<number>(this.currentRecurringTask.amount, [Validators.required]),
+      description: new FormControl<string>(this.currentRecurringTask.description)
     });
   }
 
   getExpenseGroups(userId: number) {
     if (userId <= 0) return;
-    this.expenseCategoryService.getUserCategories(userId).subscribe({
+    this._expenseCategoryService.getUserCategories(userId).subscribe({
       next: ((categories) => {
         categories.forEach((category) => {
           const group: { id: number, name: string } = {
@@ -82,14 +107,14 @@ export class EditRecurringTaskComponent implements OnInit {
       }),
       error: (error) => {
         console.log(error);
-        this.toastr.error('Ausgaben Kategorien konnten nicht geladen werden', 'Kategorien');
+        this._toastr.error('Ausgaben Kategorien konnten nicht geladen werden', 'Kategorien');
       }
     });
   }
 
   getRevenueGroups(userId: number) {
     if (userId <= 0) return;
-    this.revenueCategoryService.getUserRevenueCategories(userId).subscribe({
+    this._revenueCategoryService.getUserRevenueCategories(userId).subscribe({
       next: ((categories) => {
         categories.forEach((category) => {
           const group: { id: number, name: string } = {
@@ -101,26 +126,26 @@ export class EditRecurringTaskComponent implements OnInit {
       }),
       error: (error) => {
         console.log(error);
-        this.toastr.error('Einnahmen Kategorien konnten nicht geladen werden', 'Kategorien');
+        this._toastr.error('Einnahmen Kategorien konnten nicht geladen werden', 'Kategorien');
       }
     });
   }
 
   onClose() {
-    this.dialogRef.close();
+    this._dialogRef.close();
   }
 
   onSubmit() {
     const recurringTask: RecurringTask = this.recurringTaskForm.value as RecurringTask;
-    this.recurringTaskService.updateRecurringTask(recurringTask).subscribe({
+    this._recurringTaskService.updateRecurringTask(recurringTask).subscribe({
       next: ((response) => {
         if (response) {
-          this.toastr.success('Dauerauftrag erfolgreich bearbeitet', 'Daueraufträge');
-          this.dialogRef.close('update');
+          this._toastr.success('Dauerauftrag erfolgreich bearbeitet', 'Daueraufträge');
+          this._dialogRef.close('update');
         }
       }),
       error: (error) => {
-        this.toastr.error(error.error, 'Daueraufträge');
+        this._toastr.error(error.error, 'Daueraufträge');
         this.onClose();
       }
     });
