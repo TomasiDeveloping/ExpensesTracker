dc1a332bdf89cbbeef5fec9813f357e26a4e0186
Tomasi - Developing <info@tomasi-developing.ch>
2023-04-23 18:32:08 +0200

    CleanUp and Refactoring interceptors

--- ExpensesTracker/ClientApp/src/app/interceptors/spinner.interceptor.ts
+++ ExpensesTracker/ClientApp/src/app/interceptors/spinner.interceptor.ts
@@ -1,24 +1,18 @@
-import {Injectable} from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
-} from '@angular/common/http';
+import {inject, Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {finalize, Observable} from 'rxjs';
 import {SpinnerService} from "../services/spinner.service";
 
 @Injectable()
 export class SpinnerInterceptor implements HttpInterceptor {
 
-  constructor(private spinnerService: SpinnerService) {
-  }
+  private readonly _spinnerService = inject(SpinnerService);
 
   intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    this.spinnerService.busy();
+    this._spinnerService.busy();
     return next.handle(req).pipe(
       finalize(() => {
-        this.spinnerService.idle();
+        this._spinnerService.idle();
       })
     );
   }
